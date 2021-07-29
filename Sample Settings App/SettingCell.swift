19f19100f9b2d315fbc2f090db7c3188b1902205
tetraprism95 <nchundev99@gmail.com>
2021-07-29 18:00:53 -0400

    Setup TableView and add settings model

---
+++ Sample Settings App/SettingCell.swift
@@ -0,0 +1,23 @@
+//
+//  SettingCell.swift
+//  Sample Settings App
+//
+//  Created by Nuri Chun on 7/29/21.
+//
+
+import UIKit
+
+class SettingCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Sample Settings App/ViewController.swift
+++ Sample Settings App/ViewController.swift
@@ -7,13 +7,57 @@
 
 import UIKit
 
+struct SettingsOption {
+    let title: String
+    let icon: UIImage?
+    let iconBackgroundColor: UIColor
+    let handler: () -> Void
+}
+
 class ViewController: UIViewController {
 
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero, style: .grouped)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return tableView
+    }()
+    
+    var models = [SettingsOption]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configure()
         view.backgroundColor = .cyan
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.frame = view.bounds
     }
-
-
+    
+    private func configure() {
+        self.models = Array(0...100).compactMap({
+            SettingsOption(title: "Item \($0)", icon: UIImage(systemName: "house"), iconBackgroundColor: .systemPink, handler: {
+                
+            })
+        })
+    }
 }
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+        
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = model.title
+        
+        return cell
+    }
+}
+
 
