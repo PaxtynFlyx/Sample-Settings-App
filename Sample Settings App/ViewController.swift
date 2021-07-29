93dbf0492549451c771388628df9f24c7f48e5a9
tetraprism95 <nchundev99@gmail.com>
2021-07-29 14:53:28 -0400

    Initial Commit

---
+++ Sample Settings App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sample Settings App
+//
+//  Created by Nuri Chun on 7/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
